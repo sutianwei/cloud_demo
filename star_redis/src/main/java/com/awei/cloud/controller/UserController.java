ac808cf2023fb68f771e37335cd4d81c0d314eaa
sutianwei <18700871054@163.com>
2020-10-13 19:42:43 +0800

    redis

---
+++ star_redis/src/main/java/com/awei/cloud/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.awei.cloud.controller;
+
+
+import com.awei.cloud.entity.User;
+import com.awei.cloud.utils.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private RedisUtil redisUtil;
+
+    @RequestMapping("/set")
+    public String set() {
+        User user = new User();
+        user.setId(1);
+        user.setUserName("su");
+        user.setPassword("123");
+        redisUtil.set("user", user);
+        return "success saved user !";
+    }
+
+    @RequestMapping("/get")
+    public String get() {
+        User user = (User) redisUtil.get("user");
+        return user.toString();
+    }
+}
---
+++ star_redis/src/main/java/com/awei/cloud/entity/User.java
@@ -0,0 +1,33 @@
+package com.awei.cloud.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private int id;
+    private String userName;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
